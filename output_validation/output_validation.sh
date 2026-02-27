650397499e39a19637fd4f2b7efc5ab8f9f4585d
dah0303 <daniel.hartman@doh.wa.gov>
2026-02-27 14:52:29 -0800

    adding validation shell script

---
+++ output_validation/output_validation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/NW-PaGe/lineage_classifications/refs/heads/DOH-ALS6303-patch-13/data/lineage_classifications.csv > legacy.csv
+curl https://raw.githubusercontent.com/NW-PaGe/lineage-classification-cdc/refs/heads/test-pango-corrector/lineage_class.csv?token=GHSAT0AAAAAADM22WN7HYP5Q7OBS522PK722NCFLRQ > new.csv
+NOW=$(date +"%Y-%m-%d_%H-%M-%S")
+diff legacy.csv new.csv | echo > "full_diff_$NOW.csv"
+diff --brief legacy.csv new.csv | echo > "only_discrepancies_$NOW.csv"
