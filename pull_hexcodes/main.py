ccf909234087c565107e33912f98a6877f28778b
dah0303 <daniel.hartman@doh.wa.gov>
2026-05-29 14:18:42 -0700

    add errors and exceptions to variant pull

--- pull_hexcodes/main.py
+++ pull_hexcodes/main.py
@@ -3,21 +3,29 @@ from bs4 import BeautifulSoup
 import polars as pl
 import zipfile
 
+
 def main():
     print("Hello from test-hex-pull!")
 
     def get_variant_list():
+        print(f" \nGetting variant list from full CDC dataset \n")
         url = "https://data.cdc.gov/resource/jr58-6ysp.json"
-        query = {
-            "$query": "SELECT DISTINCT variant"
-        }
-        response = requests.get(url=url, params=query)
-        if response.status_code == 200:
-            print("dataset was pulled")
+        query = {"$query": "SELECT DISTINCT variant"}
+        try:
+            response = requests.get(url=url, params=query)
+            response.raise_for_status
+        except requests.exceptions.HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except requests.exceptions.ConnectionError as conn_err:
+            print(f"Connection error occurred: {conn_err}")
+        except requests.exceptions.Timeout as timeout_err:
+            print(f"The request timed out: {timeout_err}")
+        except requests.exceptions.RequestException as err:
+            print(f"An unexpected error occurred: {err}")
+        else:
             data = pl.from_dicts(response.json())
             unique_vars = data.unique()
-        else:
-            print(f"Error: {response.status_code}")
+            print(f"\t Success! There are {len(unique_vars)} unique variants. \n")
         return unique_vars
 
     lineage_list = get_variant_list()
@@ -59,15 +67,15 @@ def main():
 
     # create Polars DataFrame
     df = (
-        pl.DataFrame(data, schema=["variant", "color"], orient='row')
+        pl.DataFrame(data, schema=["variant", "color"], orient="row")
         # remove rows where the variant is not in the lineage list pulled from dataset
-        .filter(pl.col('variant').is_in(lineage_list["variant"].to_list()))
-
+        .filter(pl.col("variant").is_in(lineage_list["variant"].to_list()))
     )
 
     print(df)
 
     df.write_csv("parsed_hexcodes.csv")
+
 
 if __name__ == "__main__":
     main()
